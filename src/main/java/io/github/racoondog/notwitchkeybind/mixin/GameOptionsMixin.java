a7aec345fb0113b2082abaf2d083bc76e7380b68
auto <31675260+airplane@users.noreply.github.com>
2023-09-12 10:12:46 -0400

    Fix unimined stuff

--- src/main/java/io/github/racoondog/notwitchkeybind/mixin/GameOptionsMixin.java
+++ src/main/java/io/github/racoondog/notwitchkeybind/mixin/GameOptionsMixin.java
@@ -1,10 +1,8 @@
 package io.github.racoondog.notwitchkeybind.mixin;
 
 import com.google.common.collect.Lists;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.minecraft.client.option.GameOptions;
-import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.settings.GameSettings;
+import net.minecraft.client.settings.KeyBinding;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,31 +13,30 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-@Environment(EnvType.CLIENT)
-@Mixin(GameOptions.class)
+@Mixin(GameSettings.class)
 public abstract class GameOptionsMixin {
-    @Shadow public KeyBinding[] allKeys;
+    @Shadow public KeyBinding[] keyBindings;
 
-    @Shadow public KeyBinding streamStartStopKey;
-    @Shadow public KeyBinding streamPauseUnpauseKey;
-    @Shadow public KeyBinding streamCommercialKey;
-    @Shadow public KeyBinding streamToggleMicKey;
+    @Shadow public KeyBinding keyBindStreamStartStop;
+    @Shadow public KeyBinding keyBindStreamPauseUnpause;
+    @Shadow public KeyBinding keyBindStreamCommercials;
+    @Shadow public KeyBinding keyBindStreamToggleMic;
 
-    @Inject(method = "load", at = @At("HEAD"))
+    @Inject(method = "loadOptions", at = @At("HEAD"))
     private void removeKeybinds(CallbackInfo ci) {
-        List<KeyBinding> newKeys = Lists.newArrayList(allKeys);
-        newKeys.remove(streamStartStopKey);
-        newKeys.remove(streamPauseUnpauseKey);
-        newKeys.remove(streamCommercialKey);
-        newKeys.remove(streamToggleMicKey);
-        allKeys = newKeys.toArray(new KeyBinding[0]);
+        List<KeyBinding> newKeys = Lists.newArrayList(keyBindings);
+        newKeys.remove(keyBindStreamStartStop);
+        newKeys.remove(keyBindStreamPauseUnpause);
+        newKeys.remove(keyBindStreamCommercials);
+        newKeys.remove(keyBindStreamToggleMic);
+        keyBindings = newKeys.toArray(new KeyBinding[0]);
 
         Set<String> newCategories = new HashSet<>();
         for (KeyBinding keyBinding : newKeys) {
-            newCategories.add(keyBinding.getCategory());
+            newCategories.add(keyBinding.getKeyCategory());
         }
 
-        KeyBinding.getCategories().clear();
-        KeyBinding.getCategories().addAll(newCategories);
+        KeyBinding.getKeybinds().clear();
+        KeyBinding.getKeybinds().addAll(newCategories);
     }
 }
--- src/main/java/io/github/racoondog/notwitchkeybind/mixin/MinecraftClientMixin.java
+++ src/main/java/io/github/racoondog/notwitchkeybind/mixin/MinecraftClientMixin.java
@@ -1,16 +1,14 @@
 package io.github.racoondog.notwitchkeybind.mixin;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gl.Framebuffer;
-import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.options.ControlsOptionsScreen;
-import net.minecraft.client.option.GameOptions;
-import net.minecraft.client.util.NullTwitchStream;
-import net.minecraft.client.util.ScreenshotUtils;
-import net.minecraft.client.util.TwitchStreamProvider;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiControls;
+import net.minecraft.client.gui.GuiIngame;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.settings.GameSettings;
+import net.minecraft.client.shader.Framebuffer;
+import net.minecraft.client.stream.IStream;
+import net.minecraft.client.stream.NullStream;
+import net.minecraft.util.ScreenShotHelper;
 import org.lwjgl.input.Keyboard;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,53 +20,53 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.io.File;
 
-@Environment(EnvType.CLIENT)
-@Mixin(MinecraftClient.class)
+
+@Mixin(Minecraft.class)
 public abstract class MinecraftClientMixin {
-    @Shadow private TwitchStreamProvider twitchStreamProvider;
-    @Shadow public GameOptions options;
-    @Shadow @Final public File runDirectory;
-    @Shadow public int width;
-    @Shadow public int height;
-    @Shadow private Framebuffer fbo;
-    @Shadow public InGameHud inGameHud;
+    @Shadow private IStream stream;
+    @Shadow public GameSettings gameSettings;
+    @Shadow @Final public File mcDataDir;
+    @Shadow public int displayWidth;
+    @Shadow public int displayHeight;
+    @Shadow private Framebuffer framebufferMc;
+    @Shadow public GuiIngame ingameGUI;
     @Shadow public abstract void toggleFullscreen();
-    @Shadow public Screen currentScreen;
+    @Shadow public GuiScreen currentScreen;
 
     @Shadow
-    public static long getTime() {
+    public static long getSystemTime() {
         throw new AssertionError();
     }
 
-    @Inject(method = "initializeGame", at = @At("HEAD"))
+    @Inject(method = "startGame", at = @At("HEAD"))
     private void earlyInitializeStream(CallbackInfo ci) {
-        twitchStreamProvider = new NullTwitchStream(null);
+        stream = new NullStream(null);
     }
 
     /**
      * @author Crosby
-     * @reason Force {@link NullTwitchStream} and remove logged error.
+     * @reason Force {@link NullStream} and remove logged error.
      */
     @Overwrite
-    private void initializeStream() {}
+    private void initStream() {}
 
     /**
      * @author Crosby
-     * @reason Dont handle stream key inputs.
+     * @reason Don't handle stream key inputs.
      */
     @SuppressWarnings("ConstantConditions")
     @Overwrite
-    public void handleKeyInput() {
+    public void dispatchKeypresses() {
         int i = Keyboard.getEventKey() == 0 ? Keyboard.getEventCharacter() : Keyboard.getEventKey();
 
         if (i == 0 || Keyboard.isRepeatEvent()) return;
-        if (currentScreen instanceof ControlsOptionsScreen && ((ControlsOptionsScreen) currentScreen).time <= getTime() - 20L) return;
+        if (currentScreen instanceof GuiControls && ((GuiControls) currentScreen).time <= getSystemTime() - 20L) return;
         if (!Keyboard.getEventKeyState()) return;
 
-        if (i == this.options.fullscreenKey.getCode()) {
+        if (i == this.gameSettings.keyBindFullscreen.getKeyCode()) {
             this.toggleFullscreen();
-        } else if (i == this.options.screenshotKey.getCode()) {
-            this.inGameHud.getChatHud().addMessage(ScreenshotUtils.saveScreenshot(this.runDirectory, this.width, this.height, this.fbo));
+        } else if (i == this.gameSettings.keyBindScreenshot.getKeyCode()) {
+            this.ingameGUI.getChatGUI().printChatMessage(ScreenShotHelper.saveScreenshot(this.mcDataDir, this.displayWidth, this.displayHeight, this.framebufferMc));
         }
     }
 }
